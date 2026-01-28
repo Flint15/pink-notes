02c9b0069fd6eac7b4e4474537852fa0231ab9f5
Flint15 <chesspace341@gmail.com>
2026-01-28 22:55:54 +0100

    Download note when click its download button

--- src/components/DropDownMenu.tsx
+++ src/components/DropDownMenu.tsx
@@ -28,7 +28,20 @@ export default function DropDownMenu({
   };
 
   const downloadNote = (): void => {
-    console.log("love");
+    const content = notes.find((note) => note.id === currentNoteId)?.content;
+    if (!content) return;
+
+    const blob = new Blob([content], { type: "text/plain" });
+    const url = URL.createObjectURL(blob);
+    const link = document.createElement("a");
+    link.href = url;
+    link.download = "note.txt";
+
+    document.body.appendChild(link);
+    link.click();
+
+    document.body.removeChild(link);
+    URL.revokeObjectURL(url);
   };
 
   return (
