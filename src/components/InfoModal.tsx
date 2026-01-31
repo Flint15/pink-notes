49ceeac6b2e40a8f562847db1eabcf52de933851
Flint15 <chesspace341@gmail.com>
2026-01-31 07:38:09 +0100

    refactored (InfoModal): add project info and link to GitHub

--- src/components/InfoModal.tsx
+++ src/components/InfoModal.tsx
@@ -17,9 +17,17 @@ export default function InfoModal({
           e.stopPropagation();
         }}
       >
-        Your notes are stored locally in your browser. They won't sync across
-        devices, and will be deleted if you clear your browser data. Export your
-        notes regularly to back them up.
+        <div className="info-storage">
+          Your notes are stored locally in your browser. They won't sync across
+          devices, and will be deleted if you clear your browser data. Export
+          your notes regularly to back them up.
+        </div>
+        <div className="info-source">
+          Pink Notes is open source. View on GitHub -
+          <a href="https://github.com/Flint15/pink-notes" target="_blank">
+            ≽^•⩊•^≼
+          </a>
+        </div>
       </div>
     </div>
   );
