9b55bb1c93420af403446776f6482fd528e281ed
Flint15 <chesspace341@gmail.com>
2026-05-12 03:52:22 +0200

    (InfoModal.tsx): changed description

--- src/components/InfoModal.tsx
+++ src/components/InfoModal.tsx
@@ -18,9 +18,8 @@ export default function InfoModal({
         }}
       >
         <div className="info-storage">
-          Your notes are stored locally in your browser. They won't sync across
-          devices, and will be deleted if you clear your browser data. Export
-          your notes regularly to back them up.
+          Notes are tied to your user account. You can still export individual
+          notes or all notes as a `.zip` for local backups.
         </div>
         <div className="info-source">
           Pink Notes is open source. View on GitHub -
